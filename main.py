9576c88cbe082ea7a832a381dbbe93918af7ae25
Christian Ribeaud <christian@ribeaud.ch>
2018-01-06 16:31:37 +0100

    Start iteration with zero

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from itertools import repeat
+
 import unicodecsv as csv
 from ortools.graph import pywrapgraph
 
@@ -11,7 +13,8 @@ class Course:
         return str(self.__class__.__name__) + ": " + str(self.__dict__)
 
 class Participant:
-    def __init__(self, name, courses):
+    def __init__(self, id, name, courses):
+        self.id = id
         self.name = name
         self.courses = [int(course) for course in courses.split(',')]
 
@@ -26,6 +29,7 @@ def load_courses():
         reader.next()
         for row in reader:
             courses.append(Course(int(row[0]), row[1], row[2]))
+    courses.sort(key=lambda course: course.id)
     return courses
 
 def load_participants():
@@ -34,15 +38,49 @@ def load_participants():
         reader = csv.reader(csvfile, delimiter='\t')
         # Ignore header
         reader.next()
+        index = 0
         for row in reader:
-            participants.append(Participant(row[0], row[1]))
+            participants.append(Participant(index, row[0], row[1]))
+            index += 1
     return participants
+
+def create_cost_array(participants, courses_length):
+    cost = []
+    for participant in participants:
+        temp = fill(courses_length, 'NA')
+        # Assuming we go over all the courses this way
+        for course in range(courses_length):
+            if course in participant.courses:
+                temp[course] = participant.courses.index(course)
+        cost.append(temp)
+    return cost
+
+def fill(length, value):
+    return list(repeat(value, length))
 
 def main():
     courses = load_courses()
     participants = load_participants()
-    min_cost_flow = pywrapgraph.SimpleMinCostFlow()
-    print 'Hello'
+    cost = create_cost_array(participants, len(courses))
+    print cost
+    rows = len(cost)
+    cols = len(cost[0])
+    assignment = pywrapgraph.LinearSumAssignment()
+    for worker in range(rows):
+        for task in range(cols):
+            if cost[worker][task] != 'NA':
+                assignment.AddArcWithCost(worker, task, cost[worker][task])
+    solve_status = assignment.Solve()
+    if solve_status == assignment.OPTIMAL:
+        print('Total cost = ', assignment.OptimalCost())
+        print()
+        for i in range(0, assignment.NumNodes()):
+            print('Worker %d assigned to task %d.  Cost = %d' % (i, assignment.RightMate(i), assignment.AssignmentCost(i)))
+    elif solve_status == assignment.INFEASIBLE:
+        print('No assignment is possible.')
+    elif solve_status == assignment.POSSIBLE_OVERFLOW:
+        print('Some input costs are too large and may cause an integer overflow.')
+    print 'Finished!'
 
 if __name__ == '__main__':
     main()
