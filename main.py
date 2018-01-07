490fd04afe948b15a05b5adc6cc21c68853c3560
Christian Ribeaud <christian@ribeaud.ch>
2018-01-07 15:27:19 +0100

    Do some renaming and be more verbose in the output

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from parser import default_read_and_filter_csv
 
 class Course:
     '''
-    A course. A course has an unique ID (a number), a title and a maximum number of participants.
-    For each possible participant, we should have a numbered node. And first come first serve. It means, the cost
-    should be higher for the final participants.
+    A course. A course has an unique ID (a number), a title and a maximum number of students.
+    For each possible student/place, we should have a numbered node. And first come first serve. It means, the cost
+    should be higher for the final students.
     '''
-    def __init__(self, id, title, max_participants):
+    def __init__(self, id, title, max_students):
         self.id = id
         self.title = title
-        self.free = self.max_participants = max_participants
+        # The number of free places left.
+        self.free = self.max_students = max_students
 
     def __repr__(self):
         return str(self.__class__.__name__) + ": " + str(self.__dict__)
@@ -29,52 +30,69 @@ class Participant:
 def load_courses():
     courses = []
     lines = default_read_and_filter_csv('courses.txt')
+    node = 0
     for line in lines:
-        courses.append(Course(int(line['id']), line['title'], line['max_participants']))
+        max_students = int(line['max_students'])
+        course = Course(int(line['id']), line['title'].strip(), max_students)
+        to = node + max_students
+        course.nodes = range(node, to)
+        courses.append(course)
+        node = to
     courses.sort(key=lambda course: course.id)
     return courses
 
-def load_participants():
+def load_students():
     participants = []
-    lines = default_read_and_filter_csv('participants.txt')
+    lines = default_read_and_filter_csv('students.txt')
     index = 0
     for line in lines:
         participants.append(Participant(index, line['name'], line['prioritized_list']))
         index += 1
     return participants
 
-def create_cost_array(participants, courses_length):
-    cost = []
+def create_costs(participants, courses_length):
+    costs = []
     for participant in participants:
-        temp = fill(courses_length, 'NA')
+        cost = fill(courses_length, 'NA')
         # Assuming we go over all the courses this way
         for course in range(courses_length):
             if course in participant.courses:
-                temp[course] = participant.courses.index(course)
-        cost.append(temp)
-    return cost
+                cost[course] = participant.courses.index(course)
+        costs.append(cost)
+    return costs
 
 def fill(length, value):
     return list(repeat(value, length))
 
+def get_course(courses, node):
+    '''
+    For given index returns corresponding course.
+    '''
+    filtered = filter(lambda course: course.id == node, courses)
+    return filtered[0]
+
+def get_student(students, index):
+    return students[index]
+
 def main():
     courses = load_courses()
-    participants = load_participants()
-    cost = create_cost_array(participants, len(courses))
-    print cost
-    rows = len(cost)
-    cols = len(cost[0])
+    students = load_students()
+    costs = create_costs(students, len(courses))
+    print costs
+    rows = len(costs)
+    cols = len(costs[0])
     assignment = pywrapgraph.LinearSumAssignment()
-    for worker in range(rows):
-        for task in range(cols):
-            if cost[worker][task] != 'NA':
-                assignment.AddArcWithCost(worker, task, cost[worker][task])
+    for student in range(rows):
+        for course in range(cols):
+            if costs[student][course] != 'NA':
+                assignment.AddArcWithCost(student, course, costs[student][course])
     solve_status = assignment.Solve()
     if solve_status == assignment.OPTIMAL:
         print 'Total cost = %d' % assignment.OptimalCost()
-        print
         for i in range(0, assignment.NumNodes()):
-            print 'Worker %d assigned to task %d. Cost = %d' % (i, assignment.RightMate(i), assignment.AssignmentCost(i))
+            crse = get_course(courses, assignment.RightMate(i))
+            std = get_student(students, i)
+            print "Student '%s' assigned to course '%s' (%d). Cost = %d." % (std.name, crse.title, crse.id, assignment.AssignmentCost(i))
     elif solve_status == assignment.INFEASIBLE:
         print 'No assignment is possible.'
     elif solve_status == assignment.POSSIBLE_OVERFLOW:
