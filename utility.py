ef6b5b6c8951bc2cef073bd1df63c4ba6b576ec7
Christian Ribeaud <christian@ribeaud.ch>
2018-06-05 03:43:32 +0200

    Implement sorting by level followed by name

--- utility.py
+++ utility.py
@@ -1,14 +1,31 @@
 from itertools import repeat
 
-
 def fill(length, value):
     return list(repeat(value, length))
 
 def sort_students(students):
     '''
-    Sorts students by his level first, then by his name
+    Sorts students by their level first (and if present), then by their name
     '''
-    def by_level_and_name(student):
-        return (student.level, student.name) if hasattr(student, 'level') else student.name
+    def by_level_then_name(stu1, stu2):
+        if hasattr(stu1, 'level'):
+            assert hasattr(stu2, 'level'), "Must have attribute 'level' as well"
+            prepend = 'schule-'
+            l1 = stu1.level[stu1.level.index(prepend):]
+            l2 = stu2.level[stu2.level.index(prepend):]
+            return cmp(natsort_key(l1), natsort_key(l2)) or cmp(stu1.name, stu2.name)
+        else:
+            return cmp(stu1.name, stu2.name)
 
-    return sorted(students, key=by_level_and_name)
+    return sorted(students, cmp=by_level_then_name)
+
+def natsort_key(s):
+    '''
+    Used internally to get a tuple by which s is sorted.
+    '''
+    import re
+    def try_int(s):
+        "Convert to integer if possible."
+        try: return int(s)
+        except: return s
+    return map(try_int, re.findall(r'(\d+|\D+)', s))
--- utility_test.py
+++ utility_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import random
 import unittest
 
 import utility
@@ -8,22 +9,33 @@ from loader import CsvLoader
 
 class UtilityTest(unittest.TestCase):
 
-    def test_sort_students(self):
+    def setUp(self):
         # Get dummy logger for tests
-        logger = logging.getLogger()
-        logger.disabled = True
-        loader = CsvLoader(logger)
-        before = loader.load_students();
-        names = [student.name for student in before]
-        self.assertEqual(len(before), 17)
+        self.logger = logging.getLogger()
+        self.logger.disabled = True
+        loader = CsvLoader(self.logger)
+        self.students = loader.load_students()
+        self.assertEqual(len(self.students), 17)
+
+    def test_sort_students(self):
+        names = [student.name for student in self.students]
         self.assertEqual(names[0], u'Philomène')
         self.assertEqual(names[5], u'Nicolas')
         self.assertEqual(names[12], u'Lourdes')
-        after = utility.sort_students(before)
+        after = utility.sort_students(self.students)
         names = [student.name for student in after]
         self.assertEqual(names[0], u'Albert')
         self.assertEqual(names[5], u'Frédéric')
         self.assertEqual(names[12], u'Nicolas')
+
+    def test_sort_students_with_level(self):
+        levels = ['schule-5a', 'schule-5c', 'schule-10a', 'schule-9b', 'schule-7c']
+        students = list(self.students)
+        for student in students:
+            student.level = random.choice(levels)
+        after = utility.sort_students(students)
+        names = [(student.name, student.level) for student in after]
+        print(names)
 
 if __name__ == '__main__':
     unittest.main()
